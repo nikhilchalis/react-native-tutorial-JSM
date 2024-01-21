5ef3641086d84e3bb7b0a9f1a1b698976c1f6a4e
nikhilchalis <nikhilchalis@gmail.com>
2024-01-21 11:46:29 +1100

    minor changes

--- tutorial-app/App.js
+++ tutorial-app/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from '../app/home.js';
 
 export default function App() {
   return (
