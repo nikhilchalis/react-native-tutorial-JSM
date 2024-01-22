5236315abe59d89a09249c9e81c7e3e2abbfc082
nikhilchalis <nikhilchalis@gmail.com>
2024-01-22 12:28:20 +1100

    making changes to actually display

--- tutorial-app/App.js
+++ tutorial-app/App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from '../app/home.js';
+import Home from './app/home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    /*<View style={styles.container}>
+      <Text>Hi</Text>
       <StatusBar style="auto" />
-    </View>
+    </View>*/
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
 
--- tutorial-app/app/home.js
+++ tutorial-app/app/home.js
@@ -1,21 +1,29 @@
 import { useState } from "react";
 import { View, Scrollview, SafeAreaView } from 'react-native';
-import { Stack, useRouter } from 'expo-router'
+import { Stack, useRouter } from 'expo-router';
+import { StyleSheet, Text } from 'react-native';
 
 import { COLORS, icons, images, SIZES } from "../constants";
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+
 
 const Home = () => {
     const Router = useRouter();
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
-            <Stack.Screen 
-                screenOptions={{
-                    headerStyle: { backgroundColor: COLORS.lightWhite },
-                }}
-            />
-        </SafeAreaView>
-    )
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Stack />
+            <Text>Home Screen</Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 
 export default Home;
